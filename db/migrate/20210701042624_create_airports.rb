06b67746dac59a43b8bb32e3f1ac02b8d25127ff
joshua braclett <joshua.brackett@hotmail.ca>
2021-07-01 00:26:52 -0400

    added new Airline model

--- db/migrate/20210701042624_create_airports.rb
+++ db/migrate/20210701042624_create_airports.rb
@@ -2,7 +2,7 @@ class CreateAirports < ActiveRecord::Migration[6.1]
   def change
     create_table :airports do |t|
       t.string :name
-      t.reference :flights
+      t.references :flight, null: false, foreign_key: true
 
       t.timestamps
     end
